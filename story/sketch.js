f2d2f6cfea76119b83c8eb506fb6f7f323c6484e
Adonis <tommyrocks08@gmail.com>
2019-09-05 18:55:23 -0400

    added stuff

---
+++ story/sketch.js
@@ -0,0 +1,28 @@
+/*
+story sketch
+9.5.2019
+Tommy Rosemond
+mmp310
+*/
+var x = 100;
+var y = 200;
+var faceSize = 200;
+var eyeSize = 50;
+var eyeOffset
+
+
+var greeting = "Hello!";
+function setup(){
+    createCanvas(windowWidth,600);
+    background('black');
+    
+    fill(c);
+    ellipse(x,y,faceSize);
+    fill('black');
+    stroke('white');
+    s
+    
+    
+    textSize(50);
+    text(greeting,x,y);
+}
