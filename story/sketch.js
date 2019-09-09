1a5fbf547d48dc4ef0f8799b04f2066d4ec9d0b2
Adonis <tommyrocks08@gmail.com>
2019-09-09 19:20:47 -0400

    Made Character...

--- story/sketch.js
+++ story/sketch.js
@@ -8,21 +8,39 @@ var x = 100;
 var y = 200;
 var faceSize = 200;
 var eyeSize = 50;
-var eyeOffset
+/* Characters Name is Jared*/
 
 
-var greeting = "Hello!";
+var greeting = "Wassup!";
 function setup(){
     createCanvas(windowWidth,600);
     background('black');
     
-    fill(c);
+    fill('tan');
     ellipse(x,y,faceSize);
     fill('black');
     stroke('white');
-    s
+fill('white')    
+ellipse(50, 180, 30, 50);
+fill('black')
+ellipse(50, 190, 20, 30);
+fill('white')
+ellipse(150, 180, 30, 50);
+fill('black')
+ellipse(150, 190, 20, 30);
+   
+    noFill();
+strokeWeight(10.0);
+strokeJoin(ROUND);
+beginShape();
+vertex(x,y+10);
+vertex(x+20,y-10);
+vertex(x,y-20);
+endShape();
     
+    arc(100, 240, 50, 50, 50, PI + QUARTER_PI);
     
+    fill('red')
     textSize(50);
-    text(greeting,x,y);
+    text(greeting,400,y);
 }
